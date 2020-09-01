b53cd56f3afa255a07c280868f334d619e7aa4cd
Le Quang Duy <43492067+Iamthankyou@users.noreply.github.com>
2020-09-02 06:21:22 +0700

    Trigger check condition point class

--- Database/Exercies01.sql
+++ Database/Exercies01.sql
@@ -1,3 +1,26 @@
 use BT2
 
-SELECT * FROM 
+SELECT * FROM dbo.DIEM
+
+
+
+CREATE TRIGGER tgDiem ON dbo.DIEM FOR INSERT,UPDATE AS
+BEGIN
+	DECLARE @ly int,@toan int, @van int, @hoa int
+	SELECT @ly = LY from inserted
+	SELECT @toan = TOAN from inserted
+	SELECT @hoa = HOA from inserted
+	SELECT @van = VAN from inserted
+
+	if ((@ly>10 or @ly<0) or (@hoa>10 or @hoa<0) or (@van>10 or @van<0) or (@toan>10 or @toan<0)) 
+		BEGIN
+			print('Not rule!')
+			rollback transaction
+		END
+END
+
+DROP TRIGGER tgDIEM
+
+INSERT INTO dbo.DIEM(MAHS,TOAN,LY,HOA,VAN) VALUES('0101',1,1,11,1)
+
+SELECT * FROM dbo.DIEM WHERE MAHS LIKE '0101'
