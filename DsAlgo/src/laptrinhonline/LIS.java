32da5ae9c99f6c1fa938d1513f64540f2a359d97
iamthankyou <quangduynb00@gmail.com>
2020-09-03 21:06:51 +0700

    LIS with binary Search

--- DsAlgo/src/laptrinhonline/LIS.java
+++ DsAlgo/src/laptrinhonline/LIS.java
@@ -1,5 +1,53 @@
 package laptrinhonline;
 
-public class LIS {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
+public class LIS {
+	private ArrayList<Integer> res;
+	private int n;
+	
+	public int process() {
+		try(Scanner sc = new Scanner(System.in)){
+			n = sc.nextInt();
+			res = new ArrayList<>();
+			res.add(1<<31);
+			
+			for (int i=0; i<n; i++) {
+				int x = sc.nextInt();
+				
+				if (res.get(res.size()-1)<x) {
+					res.add(x);
+				}
+				else {
+					int pos = Collections.binarySearch(res, x);
+					
+					if (pos<0) {
+						pos = -(pos+1);
+					}
+					
+					if (pos>=res.size()) {
+						res.add(x);
+					}
+					else {
+						res.set(pos, x);
+					}
+				}
+			}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println(res);
+		
+		return res.size()-1;
+	}
+	
+	public static void main(String args[]) {
+		LIS lis = new LIS();
+		
+		System.out.println(lis.process());
+	}
 }
