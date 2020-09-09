cadea5230d090f507853354512f6b1dc6c0758a7
Le Quang Duy <43492067+Iamthankyou@users.noreply.github.com>
2020-09-09 09:11:55 +0700

    Exercise 05 c# ez

---
+++ C#/SourceCode/Ex5/Ex5/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Ex5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxNumber = new System.Windows.Forms.ListBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxNumber
+            // 
+            this.listBoxNumber.FormattingEnabled = true;
+            this.listBoxNumber.Location = new System.Drawing.Point(27, 30);
+            this.listBoxNumber.Name = "listBoxNumber";
+            this.listBoxNumber.Size = new System.Drawing.Size(369, 173);
+            this.listBoxNumber.TabIndex = 0;
+            this.listBoxNumber.SelectedIndexChanged += new System.EventHandler(this.listBoxNumber_SelectedIndexChanged);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(166, 228);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 1;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(425, 268);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.listBoxNumber);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxNumber;
+        private System.Windows.Forms.Button btnSubmit;
+    }
+}
+
---
+++ C#/SourceCode/Ex5/Ex5/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex5
+{
+    public partial class Form1 : Form
+    {
+        private string msg = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBoxNumber_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBoxNumber.MultiColumn = true;
+            listBoxNumber.ColumnWidth = listBoxNumber.Width/ 4;
+            listBoxNumber.SelectionMode = SelectionMode.MultiSimple;
+
+            for (int i=1; i<=100; i++)
+            {
+                listBoxNumber.Items.Add("Item " + i.ToString());
+            }
+
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            foreach (string i in listBoxNumber.SelectedItems)
+            {
+                msg += i+ " ";
+            }
+
+            MessageBox.Show(msg, "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
