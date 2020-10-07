90ce22a462e9699b5e633339eb90339592675eb7
Van An <nguyenvananymb@gmail.com>
2020-10-07 15:09:36 +0700

    bs 01

--- C#/Project/SQL/importTea.sql
+++ C#/Project/SQL/importTea.sql
@@ -9,7 +9,7 @@ INSERT INTO Resources VALUES('000003','Che Duong Nhan', 100)
 INSERT INTO Resources VALUES('000005','Hong Tra', 100)
 INSERT INTO Resources VALUES('000004','Tra Dao tui loc Phuc Long',100)
 
-INSERT INTO Resources VALUES('000005','Tran Chau', 100)
+INSERT INTO Resources VALUES('000010','Tran Chau', 100)
 INSERT INTO Resources VALUES('000006','Okinawa', 10)
 INSERT INTO Resources VALUES('000007','Hat sen tuoi', 100)
 INSERT INTO Resources VALUES('000008','Dua', 100)
