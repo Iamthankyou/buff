7d103a6fb10656830f31a8e04a7f2f99dcd5eeaa
Le Quang Duy <43492067+Iamthankyou@users.noreply.github.com>
2020-10-07 07:59:41 +0700

    Update import SQL

--- C#/Project/SQL/importTea.sql
+++ C#/Project/SQL/importTea.sql
@@ -1,1 +1,12 @@
 USE tea01
+
+SELECT * FROM Resources
+
+
+INSERT INTO Resources VALUES('000001','Hong Tra Tan Nam Bac', 100)
+INSERT INTO Resources VALUES('000002','Tuyet Yen Van Nam', 10)
+INSERT INTO Resources VALUES('000003','Che Duong Nhan', 100)
+INSERT INTO Resources VALUES('000005','Hong Tra', 100)
+INSERT INTO Resources VALUES('000004','Tra Dao tui loc Phuc Long',100)
+
+ 
