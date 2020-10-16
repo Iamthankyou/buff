584f8184888a236cac853fe465228a292c803daf
Le Quang Duy <43492067+Iamthankyou@users.noreply.github.com>
2020-10-17 01:48:11 +0700

    complete ex2

--- Database/Practice2/to.sql
+++ Database/Practice2/to.sql
@@ -11,25 +11,30 @@ SELECT * FROM SachGD
 
 /*2.Tạo view KhachVip đưa ra khách hàng gồm thông tin MaKH, TenKH, địa chỉ, điện thoại cho những khách hàng đã mua hàng với tổng tất cả các trị giá hóa đơn trong năm hiện tại lớn hơn
 30.000.000 */
-
 CREATE VIEW KhachVip AS
-SELECT tKhachHang.MaKH as 'Ma Khach Hang', (SELECT TenKH FROM tKhachHang a WHERE a.MaKH = tKhachHang.MaKH) as 'Ten Khach Hang', (SELECT DiaChi FROM tKhachHang a WHERE a.MaKH = tKhachHang.MaKH) as 'Dia chi', (SELECT DienThoai FROM tKhachHang a WHERE a.MaKH = tKhachHang.MaKH) as 'Dien thoai', SUM(SLBan*DonGiaBan) as 'Tong hoa don' FROM tKhachHang INNER JOIN tHoaDonBan ON tHoaDonBan.MaKH = tKhachHang.MaKH INNER JOIN tChiTietHDB ON tChiTietHDB.SoHDB = tHoaDonBan.SoHDB INNER JOIN tSach ON tSach.MaSach = tChiTietHDB.MaSach WHERE YEAR(NgayBan) = 2014 GROUP BY tKhachHang.MaKH HAVING SUM(SLBan*DonGiaBAn)>3000000
+SELECT tKhachHang.MaKH as 'Ma Khach Hang', (SELECT TenKH FROM tKhachHang a WHERE a.MaKH = tKhachHang.MaKH) as 'Ten Khach Hang', (SELECT DiaChi FROM tKhachHang a WHERE a.MaKH = tKhachHang.MaKH) as 'Dia chi', (SELECT DienThoai FROM tKhachHang a WHERE a.MaKH = tKhachHang.MaKH) as 'Dien thoai', SUM(SLBan*DonGiaBan*(CASE WHEN KhuyenMai IS NOT NULL THEN CONVERT(int, KhuyenMai) ELSE 1 END)) as 'Tong hoa don' FROM tKhachHang INNER JOIN tHoaDonBan ON tHoaDonBan.MaKH = tKhachHang.MaKH INNER JOIN tChiTietHDB ON tChiTietHDB.SoHDB = tHoaDonBan.SoHDB INNER JOIN tSach ON tSach.MaSach = tChiTietHDB.MaSach WHERE YEAR(NgayBan) = 2014 GROUP BY tKhachHang.MaKH HAVING SUM(SLBan*DonGiaBan*(CASE WHEN KhuyenMai IS NOT NULL THEN CONVERT(int, KhuyenMai) ELSE 1 END)) >3000000
 
 SELECT * FROM KhachVip
+
+-- Kiểm tra khuyến mại
+UPDATE tChiTietHDB SET KhuyenMai=NULL WHERE SoHDB LIKE 'HDB01'
 
 /*3.Tạo view đưa ra số hóa đơn, trị giá các hóa đơn và tổng toàn bộ trị giá của các hoa đơn do nhân viên có tên “Trần Huy” lập trong tháng hiện tại
 */
 
 CREATE VIEW TranHuy AS
 
-SELECT tHoaDonBan.SoHDB as 'So hoa don ban', SUM(SLBan*DonGiaBan) as 'Tri gia hoa don', (SELECT SUM(SLBan*DonGiaBan) as 'Tri gia hoa don' FROM tHoaDonBan INNER JOIN tNhanVien ON tHoaDonBan.MaNV = tNhanVien.MaNV INNER JOIN tChiTietHDB ON tChiTietHDB.SoHDB = tHoaDonBan.SoHDB INNER JOIN tSach ON tSach.MaSach = tChiTietHDB.MaSach WHERE TenNV LIKE N'Trần Huy' AND (MONTH(NgayBan) = 1 OR MONTH(NgayBan) = 2) GROUP BY tHoaDonBan.MaNV
+SELECT tHoaDonBan.SoHDB as 'So hoa don ban', SUM(SLBan*DonGiaBan*(CASE WHEN KhuyenMai IS NOT NULL THEN CONVERT(int, KhuyenMai) ELSE 1 END)) as 'Tri gia hoa don', (SELECT SUM(SLBan*DonGiaBan*(CASE WHEN KhuyenMai IS NOT NULL THEN CONVERT(int, KhuyenMai) ELSE 1 END)) as 'Tri gia hoa don' FROM tHoaDonBan INNER JOIN tNhanVien ON tHoaDonBan.MaNV = tNhanVien.MaNV INNER JOIN tChiTietHDB ON tChiTietHDB.SoHDB = tHoaDonBan.SoHDB INNER JOIN tSach ON tSach.MaSach = tChiTietHDB.MaSach WHERE TenNV LIKE N'Trần Huy' AND (MONTH(NgayBan) = 1 OR MONTH(NgayBan) = 2) GROUP BY tHoaDonBan.MaNV
 ) as 'Tong tri gia hoa don ban thang 1 va thang 2' FROM tHoaDonBan INNER JOIN tNhanVien ON tHoaDonBan.MaNV = tNhanVien.MaNV INNER JOIN tChiTietHDB ON tChiTietHDB.SoHDB = tHoaDonBan.SoHDB INNER JOIN tSach ON tSach.MaSach = tChiTietHDB.MaSach WHERE TenNV LIKE N'Trần Huy' AND (MONTH(NgayBan) = 1 OR MONTH(NgayBan) = 2) GROUP BY tHoaDonBan.SoHDB
 
 UNION ALL
 
-SELECT tHoaDonNhap.SoHDN as 'So hoa don nhap', SUM(SLNhap*DonGiaNhap) as 'Tri gia hoa don nhap', (SELECT SUM(SLNhap*DonGiaNhap)  FROM tHoaDonNhap INNER JOIN tNhanVien ON tHoaDonNhap.MaNV = tNhanVien.MaNV INNER JOIN tChiTietHDN ON tChiTietHDN.SoHDN = tHoaDonNhap.SoHDN INNER JOIN tSach ON tSach.MaSach = tChiTietHDN.MaSach WHERE TenNV LIKE N'Trần Huy' AND (MONTH(NgayNhap) = 1 OR MONTH(NgayNhap) = 6) GROUP BY tHoaDonNhap.MaNV
+SELECT tHoaDonNhap.SoHDN as 'So hoa don nhap', SUM(SLNhap*DonGiaNhap*(CASE WHEN KhuyenMai IS NOT NULL THEN CONVERT(int, KhuyenMai) ELSE 1 END)
+) as 'Tri gia hoa don nhap', (SELECT SUM(SLNhap*DonGiaNhap*(CASE WHEN KhuyenMai IS NOT NULL THEN CONVERT(int, KhuyenMai) ELSE 1 END)
+)  FROM tHoaDonNhap INNER JOIN tNhanVien ON tHoaDonNhap.MaNV = tNhanVien.MaNV INNER JOIN tChiTietHDN ON tChiTietHDN.SoHDN = tHoaDonNhap.SoHDN INNER JOIN tSach ON tSach.MaSach = tChiTietHDN.MaSach WHERE TenNV LIKE N'Trần Huy' AND (MONTH(NgayNhap) = 1 OR MONTH(NgayNhap) = 6) GROUP BY tHoaDonNhap.MaNV
 ) as 'Tong tri gia hoa don ban thang 1 va t6' FROM tHoaDonNhap INNER JOIN tNhanVien ON tHoaDonNhap.MaNV = tNhanVien.MaNV INNER JOIN tChiTietHDN ON tChiTietHDN.SoHDN = tHoaDonNhap.SoHDN  INNER JOIN tSach ON tSach.MaSach = tChiTietHDN.MaSach WHERE TenNV LIKE N'Trần Huy' AND (MONTH(NgayNhap) = 1 OR MONTH(NgayNhap) = 6) GROUP BY tHoaDonNhap.SoHDN
 
+SELECT * FROM TranHuy
 /*
 4. Tạo view đưa thông tin các các sách còn tồn 
 */
@@ -55,9 +60,12 @@ SELECT * FROM SACHKHONGBANDUOC2014
    [!]Do NXB Giáo Dục không có quyển nào không bán được trong năm 2014 nên lấy NXB Thủ Đô
 */
 
+CREATE VIEW sachKhongBanDuocCuaNXB AS 
 SELECT a.MaSach, a.TenSach as 'Ten sach khong ban duoc quyen nao nam 2014 cua NXB Thu Do' FROM tSach a LEFT JOIN (SELECT b.MaSach FROM tSach b INNER JOIN tChiTietHDB ON tChiTietHDB.MaSach = b.MaSach INNER JOIN tHoaDonBan ON tHoaDonBan.SoHDB = tChiTietHDB.SoHDB WHERE YEAR(NgayBan)
 = 2014
 ) as c ON c.MaSach = a.MaSach INNER JOIN tNhaXuatBan ON tNhaXuatBan.MaNXB = a.MaNXB WHERE c.MaSach IS NULL AND tNhaXuatBan.TenNXB LIKE  N'NXB Thủ Đô'
+
+SELECT * FROM sachKhongBanDuocCuaNXB
 
 /*
 7. Tạo view đưa ra các thông tin về sách và số lượng từng sách được bán ra trong năm 2014.
@@ -73,7 +81,7 @@ SELECT * FROM SoLuongSachBanRa2014
 */
 
 CREATE VIEW VIP2014 AS
-SELECT tHoaDonBan.SoHDB, (SELECT TenKH FROM tKhachHang a WHERE a.MaKH = (SELECT MaKH FROM tHoaDonBan b WHERE b.SoHDB = tHoaDonBan.SoHDB)) as 'VIP 2014', SUM(SLBan*DonGiaBan) as 's' FROM tChiTietHDB INNER JOIN tHoaDonBan ON tHoaDonBan.SoHDB = tChiTietHDB.SoHDB INNER JOIN tSach ON tSach.MaSach = tChiTietHDB.MaSach INNER JOIN tKhachHang ON tKhachHang.MaKH = tHoaDonBan.MaKH WHERE YEAR(NgayBan) = 2014 GROUP BY tHoaDonBan.SoHDB HAVING SUM(SLBan*DonGiaBan) = (SELECT MAX(d.sum) FROM (SELECT SUM(tChiTietHDB.SLBan*tSach.DonGiaBan) as 'sum' FROM tChiTietHDB INNER JOIN tHoaDonBan ON tHoaDonBan.SoHDB = tChiTietHDB.SoHDB INNER JOIN tSach ON tSach.MaSach = tChiTietHDB.MaSach WHERE YEAR(NgayBan) = 2014 GROUP BY tHoaDonBan.SoHDB) as d
+SELECT tHoaDonBan.SoHDB, (SELECT TenKH FROM tKhachHang a WHERE a.MaKH = (SELECT MaKH FROM tHoaDonBan b WHERE b.SoHDB = tHoaDonBan.SoHDB)) as 'VIP 2014', SUM(SLBan*DonGiaBan*(CASE WHEN KhuyenMai IS NOT NULL THEN CONVERT(int, KhuyenMai) ELSE 1 END)) as 's' FROM tChiTietHDB INNER JOIN tHoaDonBan ON tHoaDonBan.SoHDB = tChiTietHDB.SoHDB INNER JOIN tSach ON tSach.MaSach = tChiTietHDB.MaSach INNER JOIN tKhachHang ON tKhachHang.MaKH = tHoaDonBan.MaKH WHERE YEAR(NgayBan) = 2014 GROUP BY tHoaDonBan.SoHDB HAVING SUM(SLBan*DonGiaBan*(CASE WHEN KhuyenMai IS NOT NULL THEN CONVERT(int, KhuyenMai) ELSE 1 END)) = (SELECT MAX(d.sum) FROM (SELECT SUM(tChiTietHDB.SLBan*tSach.DonGiaBan*(CASE WHEN KhuyenMai IS NOT NULL THEN CONVERT(int, KhuyenMai) ELSE 1 END)) as 'sum' FROM tChiTietHDB INNER JOIN tHoaDonBan ON tHoaDonBan.SoHDB = tChiTietHDB.SoHDB INNER JOIN tSach ON tSach.MaSach = tChiTietHDB.MaSach WHERE YEAR(NgayBan) = 2014 GROUP BY tHoaDonBan.SoHDB) as d
 )
 
 SELECT * FROM VIP2014
@@ -84,7 +92,7 @@ tại.
 */
 
 CREATE VIEW BestEmployee AS
-SELECT TOP 3 tNhanVien.MaNV, SUM(SLBan*DonGiaBan) as 'Tong hoa don', (SELECT a.TenNV FROM tNhanVien a WHERE a.MaNV = tNhanVien.MaNV) as 'Ten Nhan Vien' FROM tHoaDonBan INNER JOIN tNhanVien ON tNhanVien.MaNV = tHoaDonBan.MaNV INNER JOIN tChiTietHDB ON tChiTietHDB.SoHDB = tHoaDonBan.SoHDB INNER JOIN tSach ON tSach.MaSach = tChiTietHDB.MaSach GROUP BY tNhanVien.MaNV ORDER BY SUM(SLBan*DonGiaBan) DESC 
+SELECT TOP 3 tNhanVien.MaNV, SUM(SLBan*DonGiaBan*(CASE WHEN KhuyenMai IS NOT NULL THEN CONVERT(int, KhuyenMai) ELSE 1 END)) as 'Tong hoa don', (SELECT a.TenNV FROM tNhanVien a WHERE a.MaNV = tNhanVien.MaNV) as 'Ten Nhan Vien' FROM tHoaDonBan INNER JOIN tNhanVien ON tNhanVien.MaNV = tHoaDonBan.MaNV INNER JOIN tChiTietHDB ON tChiTietHDB.SoHDB = tHoaDonBan.SoHDB INNER JOIN tSach ON tSach.MaSach = tChiTietHDB.MaSach GROUP BY tNhanVien.MaNV ORDER BY SUM(SLBan*DonGiaBan*(CASE WHEN KhuyenMai IS NOT NULL THEN CONVERT(int, KhuyenMai) ELSE 1 END)) DESC 
 
 SELECT * FROM BestEmployee
 
@@ -204,3 +212,8 @@ SELECT * FROM DSSinhVien INNER JOIN KetQua ON KetQua.MaSV = DSSinhVien.MaSV WHER
 10. Tạo view danh sách sinh viên không bi rớt môn nào	
 */
 
+CREATE VIEW aquamon AS
+SELECT CONCAT(b.HoSV,' ',b.TenSV) as 'Ho va ten' FROM DSSinhVien b LEFT JOIN (SELECT c.MaSV FROM DSSinhVien c INNER JOIN KetQua ON KetQua.MaSV = c.MaSV WHERE KetQua.Diem <5
+) as a ON a.MaSV = b.MaSV WHERE a.MaSV IS NULL
+
+/*====================================================================BAI3======================================================================*/
