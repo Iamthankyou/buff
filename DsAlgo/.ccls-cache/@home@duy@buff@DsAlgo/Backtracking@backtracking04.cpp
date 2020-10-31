6ea5f28160040ea89ca416da30ca56b8f8664a5a
iamthankyou <quangduynb00@gmail.com>
2020-10-31 07:14:09 +0700

    Backtracking last day

---
+++ DsAlgo/.ccls-cache/@home@duy@buff@DsAlgo/Backtracking@backtracking04.cpp
@@ -0,0 +1,7 @@
+// Problem: Dat n con hau len ban co nxn khong con nao an con nao
+#incluse <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+}
--- DsAlgo/Backtracking/backtracking02.cpp
+++ DsAlgo/Backtracking/backtracking02.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 // Problem: Nhap n phan tich thanh tong cac so duong khong giam
 
-void backTracking(int *x, int t, int n){
+void backTracking(int *x, int k, int t, int n){
   if (t==n){
     for (int i=1; i<=n; i++){
       cout << x[i] << " ";
@@ -13,8 +13,8 @@ void backTracking(int *x, int t, int n){
   }
   else{
     if (x[n]<n-t+1){
-      for (int i=n; i<=n-t+1; i++){
-        backTracking(x[i],n-t+1,n);
+      for (int i=x[n]; i<=n-t+1; i++){
+        backTracking(x,k+1,n-t+1,n);
       }
     }
   }
@@ -22,9 +22,10 @@ void backTracking(int *x, int t, int n){
 
 int main(){
   freopen("input.txt","r",stdin);
-  freopen("output.txt","w",stdout);
+//  freopen("output.txt","w",stdout);
+
   int x[1000];
 
-  backTracking(x,0,5);
+  backTracking(x,0,0,5);
 
 }
