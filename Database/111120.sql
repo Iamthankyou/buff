15510ad9b9bbb49cf2c554e31ac024455de47fe3
Le Quang Duy <43492067+Iamthankyou@users.noreply.github.com>
2020-11-29 05:24:13 +0700

    1/2 exercise Database

---
+++ Database/111120.sql
@@ -0,0 +1,47 @@
+﻿USE BT1
+
+/*Tạo trường số lượng nhập tren bảng sách, cập nhật tự động cho trường này cho cả ba hành động cả insert, update, delete*/
+
+ALTER TABLE tSach ADD SLN int
+
+DROP TRIGGER autoUpdateHDN
+
+CREATE TRIGGER autoUpdateHDN ON tChiTietHDN FOR INSERT,UPDATE,DELETE  AS
+BEGIN
+	UPDATE tSach SET SLN = ISNULL((SELECT SUM(SLNhap) FROM tChiTietHDN GROUP BY MaSach HAVING MaSach = (SELECT MaSach FROM INSERTED)),0) WHERE MaSach = ((SELECT MaSach FROM INSERTED))
+	UPDATE tSach SET SLN = ISNULL((SELECT SUM(SLNhap) FROM tChiTietHDN GROUP BY MaSach HAVING MaSach = (SELECT MaSach FROM DELETED)),0) WHERE MaSach = ((SELECT MaSach FROM DELETED))
+END
+
+-- Check
+
+INSERT INTO tHoaDonNhap(SoHDN,MaNV,NgayNhap,MaNCC) VALUES('HDN07','NV02',GETDATE(),'NCC02')
+INSERT INTO tChiTietHDN(SoHDN,MaSach,SLNhap) VALUES('HDN07','S04','5')
+
+UPDATE tChiTietHDN SET SLNhap=100 WHERE SOHDN = 'HDN07'
+
+DELETE tChiTietHDN WHERE SoHDN = 'HDN06'
+
+SELECT MaSach,SUM(SLNhap) FROM tChiTietHDN GROUP BY MaSach 
+
+SELECT SUM(SLNhap) FROM tChiTietHDN GROUP BY MaSach HAVING MaSach = tSach.MaSach
+
+SELECT * FROM tChiTietHDN
+
+UPDATE tChiTietHDN SET SLNhap=15 WHERE SoHDN = 'HDN05'
+SELECT * FROM tHoaDonNhap
+
+https://cl.lexax.ru/cr/pr2
+https://shopey.ru/
+
+SELECT * FROM tNhaXuatBan
+
+SELECT tNhaXuatBan.MaNXB as 'MaNXB',tChiTietHDB.SoHDB as 'SoHD',SLBan  as 'SL' FROM tChiTietHDB INNER JOIN tHoaDonBan ON tChiTietHDB.SoHDB=tHoaDonBan.SoHDB INNER JOIN tSach ON tSach.MaSach = tChiTietHDB.MaSach INNER JOIN tNhaXuatBan ON tNhaXuatBan.MaNXB = tSach.MaNXB WHERE tNhaXuatBan.MaNXB LIKE 'NXB01'
+UNION ALL
+SELECT tNhaXuatBan.MaNXB as 'MaNXB',tChiTietHDN.SoHDN,SLNhap FROM tChiTietHDN INNER JOIN tHoaDonNhap ON tChiTietHDN.SoHDN=tHoaDonNhap.SoHDN INNER JOIN tSach ON tSach.MaSach = tChiTietHDN.MaSach INNER JOIN tNhaXuatBan ON tNhaXuatBan.MaNXB = tSach.MaNXB WHERE tNhaXuatBan.MaNXB LIKE 'NXB01'
+
+SELECT * FROM (SELECT tNhaXuatBan.MaNXB as 'MaNXB',tChiTietHDB.SoHDB,SLBan FROM tChiTietHDB INNER JOIN tHoaDonBan ON tChiTietHDB.SoHDB=tHoaDonBan.SoHDB INNER JOIN tSach ON tSach.MaSach = tChiTietHDB.MaSach INNER JOIN tNhaXuatBan ON tNhaXuatBan.MaNXB = tSach.MaNXB WHERE tNhaXuatBan.MaNXB LIKE 'NXB01'
+) as a INNER 
+
+
+SELECT * FROM tChiTietHDN
+
---
+++ Database/Practice4/Duy_LeQuang_181203460_N01.sql
@@ -0,0 +1,72 @@
+﻿USE BT2911
+
+/*
+1. Tạo cơ sở dữ liệu trên, với các khóa chính như sau:
+ChiTietVanTai là MaVT, TrongTai là MaTrongTai, LoTrinh là MaLoTrinh 
+*/
+
+ALTER TABLE ChiTietVanTai 
+ADD CONSTRAINT PK_MaVT PRIMARY KEY (MAVT)
+
+ALTER TABLE TrongTai
+ADD CONSTRAINT PK_MaTrongTai PRIMARY KEY (MaTrongTai)
+
+ALTER TABLE LoTrinh
+ADD CONSTRAINT PK_MaLoTrinh PRIMARY KEY (MaLoTrinh)
+
+/*
+2. Tạo view gồm các trường SoXe, MaLoTrinh, SoLuongVT, NgayDi, NgayDen,
+ThoiGianVT, CuocPhi, Thuong. Trong đó:
+• ThoiGianVT: là 1 nếu vận chuyển trong ngày, là (NgayDen-NgayDi) trong trường hợp
+ngược lại.
+• CuocPhi: là SoLuongVT x DonGia x 105% nếu SoLuongVT nhiều hơn TrongTaiQD, là
+SoLuongVT x DonGia trong trường hợp ngược lại.
+• Thuong: là 5% của CuocPhi nếu ThoiGianVT vuot ThoiGianQD, là 0 trong trường hợp
+ngược lại. 
+*/
+
+CREATE VIEW View02 AS
+	SELECT SoXe,ChiTietVanTai.MaLoTrinh,SoLuongVT,NgayDi,NgayDen,DATEDIFF(DAY,NgayDi,NgayDen)+1 as 'ThoiGianVT',dbo.CuocPhi(SoLuongVT,DonGia,TrongTaiQD) as 'CuocPhi', IIF(ThoiGianQD<DATEDIFF(DAY,NgayDi,NgayDen)+1,dbo.CuocPhi(SoLuongVT,DonGia,TrongTaiQD)*0.05,0) as 'Thuong' FROM ChiTietVanTai INNER JOIN LoTrinh ON ChiTietVanTai.MaLoTrinh = LoTrinh.MaLoTrinh INNER JOIN TrongTai ON TrongTai.MaTrongTai = ChiTietVanTai.MaTrongTai
+
+SELECT * FROM View02
+
+CREATE FUNCTION CuocPhi(@SoLuongVt int,@DonGia int,@TrongTaiQD int) RETURNS money AS
+BEGIN
+	DECLARE @res money
+	IF @SoLuongVt>@TrongTaiQD
+		SET @res = @SoLuongVt*@DonGia*1.05
+	ELSE
+		SET @res = @SoLuongVt*@DonGia
+	RETURN @res
+END 
+
+/*
+3. Tạo view để lập bảng cước phí gồm các trường SoXe, TenLoTrinh, SoLuongVT, NgayDi,
+NgayDen, CuocPhi. 
+*/
+CREATE VIEW Cuoc AS
+	SELECT SoXe,TenLoTrinh,SoLuongVT,NgayDi,NgayDen,dbo.CuocPhi(SoLuongVT,DonGia,TrongTaiQD) as 'Cuoc' FROM ChiTietVanTai INNER JOIN LoTrinh ON LoTrinh.MaLoTrinh = ChiTietVanTai.MaLoTrinh INNER JOIN TrongTai ON TrongTai.MaTrongTai = ChiTietVanTai.MaTrongTai
+
+SELECT * FROM Cuoc
+
+/*
+4. Tạo view danh sách các xe có có SoLuongVT vượt trọng tải qui định, gồm các trường
+SoXe, TenLoTrinh, SoLuongVT, TronTaiQD, NgayDi, NgayDen. 
+*/
+CREATE VIEW SoLuongVanTaiVuotQuyDinh AS
+	SELECT SoXe,TenLoTrinh,SoLuongVT,TrongTaiQD,NgayDi,NgayDen FROM ChiTietVanTai INNER JOIN TrongTai ON TrongTai.MaTrongTai = ChiTietVanTai.MaTrongTai INNER JOIN LoTrinh ON LoTrinh.MaLoTrinh = ChiTietVanTai.MaLoTrinh WHERE SoLuongVT>TrongTaiQD
+
+SELECT * FROM SoLuongVanTaiVuotQuyDinh
+
+/*
+5. Tạo hàm có đầu vào là lộ trình, đầu ra là số xe, mã trọng tải, số lượng vận tải, ngày đi, ngày
+đến (SoXe, MaTrongTai, SoLuongVT, NgayDi, NgayDen.)
+*/
+
+CREATE FUNCTION exLoTrinh(@maLoTrinh nvarchar(255)) RETURNS TABLE AS
+	RETURN SELECT SoXe,TrongTai.MaTrongTai,SoLuongVT,NgayDi,NgayDen FROM ChiTietVanTai INNER JOIN LoTrinh ON ChiTietVanTai.MaLoTrinh = LoTrinh.MaLoTrinh INNER JOIN TrongTai ON ChiTietVanTai.MaTrongTai = TrongTai.MaTrongTai WHERE LoTrinh.MaLoTrinh = @maLoTrinh
+
+SELECT * FROM dbo.exLoTrinh('DN')
+
+/*
+6. Thiết lập hàm có đầu vào là số xe, đầu ra là thông tin về lộ trình*/
